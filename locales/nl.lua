dbdd47c65856ce8e5cfe8b43e3f7d408b09b2c6e
MriKNG <ayrton.buyle@hotmail.com>
2026-05-31 14:00:49 +0200

    Added dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,47 @@
+Loc = Loc or {}
+
+Loc["nl"] = {
+    error = {
+        noSpace = "Onvoldoende ruimte in je inventaris",
+        noMoney = "Onvoldoende geld",
+        incorrectAmount = "Ongeldig aantal!",
+        noItems = "Fout - Geen producten gevonden in deze winkel",
+    },
+    basket = {
+        balance = "Saldo",
+        totalCost = "Totaalprijs:",
+        itemsBasket = "Producten in het winkelmandje:",
+        clearBasket = "Winkelmandje leegmaken",
+        basketHeader = "Winkelmandje",
+        removeBasket = "Product verwijderen",
+        editAmount = "Aantal aanpassen",
+        editTxt = "Pas het aantal van dit product in je winkelmandje aan",
+        basketEmpty = "Winkelmandje is leeg",
+        itemBasket = "Winkelmandje",
+        items = "Product(en)"
+    },
+    general = {
+        free = "Gratis",
+        confirm = "Aankoop bevestigen",
+        amount = "Aantal",
+        amountToBuy = "Aantal om te kopen",
+        cost = "Prijs:",
+        costPerItem = "Prijs per product:",
+        paymentType = "Betaalmethode",
+        numProducts = "Producten",
+        goBack = "Terug",
+        weight = "Gewicht:",
+        outOfStock = "Niet op voorraad",
+        chargeSociety = "Bedrijfsrekening wordt belast voor deze aankoop",
+        freeItem = "Gratis product",
+        cash = "Contant",
+        card = "Kaart",
+        society = "Bedrijf",
+        addedToBasket = "%s product(en) toegevoegd aan het winkelmandje"
+    },
+    target = {
+        shopBlip = "Winkel",
+        sellerBlip = "Winkel",
+        browseShop = "Winkel bekijken",
+    },
+}
